8aecf392f47d16878dec482d13fc9ba7e3d7fcd1
Booogyboooo2 <spamsignup16@gmail.com>
2024-07-16 20:15:40 +0000

    added code

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
+// Usage:
+//
+// Create Modifier For The Target Class (GooseV2)
+// var gooseModifier = new MethodModif(GooseV2)
+//
+// Run Modifier To Override The Method "test" and replace with code "console.log('test!')"
+// gooseModifier.overrideMethod("test", "console.log('test!')");
+//
+// -------------------------------------------------------
+//
+// Example:
+//
+// -------------------------------------------------------
+//
+// class GooseV2 {
+//      test() {
+//          console.log("original...");
+//      }
+// }
+//
+// Result:
+// var g = new GooseV2();
+// g.test(); // Result: original...
+// var gooseModifier = new MethodModif(GooseV2)
+// gooseModifier.overrideMethod("test", "console.log('test!')");
+// g.test(); // Result: test!
+
 class MethodModif {
     //Use class or function for modification
     constructor(target) {
         this.target = target;
     }
 
+    //Override the method
     overrideMethod(method, code) {
         try {
             eval(this.target + ".prototype." + method + " = function () {" + code + "}");
@@ -12,6 +40,7 @@ class MethodModif {
         }
     }
 
+    //Add code at the end of the method
     injectMethod(method, code) {
         try {
             var prev_1 = this.target + '.prototype[method].toString().replaceAll("\n", "").replaceAll(" ", "").replace(method + "(){", "").slice(0, -1).replaceAll("elseif", "else if")';
