22f3d062dceba76d4b4db58f72b58dc5f8ffbb37
Booogyboooo2 <spamsignup16@gmail.com>
2024-07-16 20:02:30 +0000

    a

---
+++ main.js
@@ -0,0 +1,27 @@
+class MethodModif {
+    //Use class or function for modification
+    constructor(target) {
+        this.target = target;
+    }
+
+    overrideMethod(method, code) {
+        try {
+            eval(this.target + ".prototype." + method + " = function () {" + code + "}");
+        } catch (error) {
+            console.log("Error (" + error + ") while trying to override method (" + method + ")");
+        }
+    }
+
+    injectMethod(method, code) {
+        try {
+            var prev_1 = this.target + '.prototype[method].toString().replaceAll("\n", "").replaceAll(" ", "").replace(method + "(){", "").slice(0, -1).replaceAll("elseif", "else if")';
+            var prev = eval(prev_1);
+            var new_1 = this.target + '".prototype." + method + " = function () { " + prev + code + "}"';
+            eval(new_1);
+            console.log("Set code too: " + this.target + ".prototype." + method + " = function () { " + prev + code + "}");
+        } catch (error) {
+            console.log("Error (" + error + ") while trying to inject into method (" + method + ")");
+            console.log("Set code too: " + this.target + ".prototype." + method + " = function () { " + prev + code + "}");
+        }
+    }
+}
